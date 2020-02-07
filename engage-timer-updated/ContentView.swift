221f08a2d97f03be37b7546093c4431021fede7c
Adam Reed <adam.rdconcepts@gmail.com>
2020-02-06 20:06:06 -0800

    Initial Commit

---
+++ engage-timer-updated/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  engage-timer-updated
+//
+//  Created by Adam Reed on 2/6/20.
+//  Copyright © 2020 rdConcepts. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
